296d3d9cbccf15f33579bcfb6aa3f5fa9ba89386
VailElla <63608800+VailElla@users.noreply.github.com>
2026-07-05 06:08:25 +0800

    Release AutoPowerMode v1.1.1

--- AppInfo.cs
+++ AppInfo.cs
@@ -3,6 +3,6 @@ namespace AutoPowerMode;
 public static class AppInfo
 {
     public const string Name = "AutoPowerMode";
-    public const string Version = "v1.0.8";
+    public const string Version = "v1.1.1";
     public const string DisplayName = $"{Name} {Version}";
 }
